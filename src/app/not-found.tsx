f3c24deb767fa4efdacd6659710b1e494d24ab4a
burak <sglk.brk@gmail.com>
2024-11-27 01:59:25 +0300

    edit not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <section className="flex h-[calc(100vh_-_90px)] w-full flex-col items-center justify-center gap-4">
       <h2>404 Not Found</h2>
-      <p className="text-gray-500">I think you've lost your way.</p>
+      <p className="text-gray-500">I think you ve lost your way.</p>
       <button>
         <Link href="/">Back to Homepage</Link>
       </button>
