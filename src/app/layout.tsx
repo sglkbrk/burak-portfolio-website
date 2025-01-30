4b7d97f35401a2b0cbffd6c658da6f39652f5e60
burak <sglk.brk@gmail.com>
2025-01-30 17:16:11 +0300

    google-analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,11 @@ import { Montserrat } from 'next/font/google';
 import Header from '@/components/Header';
 import Background from '@/components/Background';
 import Footer from '@/components/Footer';
+import Head from 'next/head';
+import Script from 'next/script';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
+
 export const metadata: Metadata = {
   title: 'Burak Sağlık | Personal',
   metadataBase: new URL('https://www.buraksaglik.com'),
@@ -13,10 +16,12 @@ export const metadata: Metadata = {
     canonical: '/'
   },
   authors: [{ name: 'Burak Sağlık', url: 'https://github.com/sglkbrk' }],
-  description: "Burak Sağlık's personal portfolio website",
+  description:
+    "Explore Burak Sağlık's personal portfolio showcasing creative web development projects, modern UI/UX designs, and dynamic applications built with React, TypeScript, and Tailwind CSS. Let's collaborate!",
   openGraph: {
     title: 'Burak Sağlık | Personal',
-    description: "Burak Sağlık's personal portfolio website",
+    description:
+      "Explore Burak Sağlık's personal portfolio showcasing creative web development projects, modern UI/UX designs, and dynamic applications built with React, TypeScript, and Tailwind CSS. Let's collaborate!",
     images: [
       {
         url: '/IMG_0419.jpeg',
@@ -29,8 +34,30 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const structuredData = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: 'Burak Sağlık',
+    jobTitle: 'Web Developer & Photographer',
+    url: 'https://buraksaglik.com/',
+    sameAs: ['https://github.com/sglkbrk', 'https://www.instagram.com/brksglk', 'https://www.linkedin.com/in/burak-saglik/']
+  };
   return (
     <html lang="en">
+      <Head>
+        <>
+          <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }} />
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-11GN8Y8ER7"></Script>
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-11GN8Y8ER7');
+        `}
+          </Script>
+        </>
+      </Head>
       <body className={montserrat.className}>
         <Header />
         <Background />
