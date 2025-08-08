bb566ddf3b1c4a513b9f8948d456a1b7d8d8a606
sglkbrk <sglk.brk@gmail.com>
2025-08-08 12:05:49 +0300

    edit project List

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -5,37 +5,49 @@ export default function Projects() {
     {
       name: 'Goalio',
       image: '/Goalio.jpg',
-      appStoreUrl: 'https://apps.apple.com/tr/app/goalio/id6749142434?l=tr'
+      appStoreUrl: 'https://apps.apple.com/tr/app/goalio/id6749142434?l=tr',
+      playStoreUrl: '',
+      projectUrl: ''
     },
     {
       name: 'Vue Chat',
       image: '/chat.png',
       githubUrl: 'https://github.com/sglkbrk/sinnalR-Chat-vueapp',
-      projectUrl: 'https://chat.buraksaglik.com/'
+      projectUrl: 'https://chat.buraksaglik.com/',
+      appStoreUrl: '',
+      playStoreUrl: ''
     },
     {
       name: 'Next Gallery',
       image: '/gallery.png',
       githubUrl: 'https://github.com/sglkbrk/next-photo-gallery',
-      projectUrl: 'https://gallery.buraksaglik.com/'
+      projectUrl: 'https://gallery.buraksaglik.com/',
+      appStoreUrl: '',
+      playStoreUrl: ''
     },
     {
       name: 'Personal Portfolio',
       image: '/portfolio.png',
       githubUrl: 'https://github.com/sglkbrk/burak-portfolio-website',
-      projectUrl: 'https://buraksaglik.com/'
+      projectUrl: 'https://buraksaglik.com/',
+      appStoreUrl: '',
+      playStoreUrl: ''
     },
     {
       name: 'Bs Note',
       image: '/bsnote.png',
       githubUrl: 'https://github.com/sglkbrk/bsnotes',
-      projectUrl: 'https://bsnote.buraksaglik.com/'
+      projectUrl: 'https://bsnote.buraksaglik.com/',
+      appStoreUrl: '',
+      playStoreUrl: ''
     },
     {
       name: 'Blog',
       image: '/blog.png',
       githubUrl: 'https://github.com/sglkbrk',
-      projectUrl: 'https://blog.buraksaglik.com/'
+      projectUrl: 'https://blog.buraksaglik.com/',
+      appStoreUrl: '',
+      playStoreUrl: ''
     }
   ];
   return (
