8ba57e84c03b874dba08c55c3e3d64ed36745914
sglkbrk <sglk.brk@gmail.com>
2025-12-08 22:30:57 +0300

    dz

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["buraksaglik.com", "www.buraksaglik.com"],
-    formats: ["image/avif", "image/webp"],
+    domains: ['buraksaglik.com', 'www.buraksaglik.com'],
+    formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 60,
+    minimumCacheTTL: 60
   },
   compress: true,
   poweredByHeader: false,
-  reactStrictMode: true,
-  swcMinify: true,
+  reactStrictMode: true
 };
 
 export default nextConfig;
