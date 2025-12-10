ac4373f32fbb369b2523d19ddcad56764a3b1ca2
sglkbrk <sglk.brk@gmail.com>
2025-12-10 17:11:34 +0300

    type edit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ['buraksaglik.com', 'www.buraksaglik.com'],
     formats: ['image/avif', 'image/webp'],
@@ -13,4 +12,4 @@ const nextConfig: NextConfig = {
   reactStrictMode: true
 };
 
-export default nextConfig;
+module.exports = nextConfig;
